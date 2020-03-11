22f3be3c6c701dca063eb009fe9e8b644cc32e85
Raphael F <raphaelf.ti@gmail.com>
2020-03-11 20:06:32 -0300

    Removed old cron files

--- cron.py
+++
@@ -1,8 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-from main import job
-
-scheduler = BlockingScheduler()
-scheduler.add_job(job, 'cron', hour=18, timezone='America/Maceio')
-
-scheduler.start()
--- utils.py
+++
@@ -1,7 +0,0 @@
-def get_key_value(key_, dict_):
-    if key_ in dict_:
-        k = int(dict_[key_])
-    else:
-        k = 0
-    return k
-
