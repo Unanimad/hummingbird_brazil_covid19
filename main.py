3283b759d81958a07fd9e1cf9fd06eb980e47b0d
Raphael F <raphaelf.ti@gmail.com>
2020-03-11 07:46:29 -0300

    Split date and hour in two columns

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ STATES = {11: 'Rondônia', 12: 'Acre', 13: 'Amazonas', 14: 'Roraima', 15: 'Pará
           24: 'Rio Grande do Norte', 25: 'Paraíba', 26: 'Pernambuco', 27: 'Alagoas',
           28: 'Sergipe', 29: 'Bahia', 31: 'Minas Gerais', 32: 'Espírito Santo',
           33: 'Rio de Janeiro', 35: 'São Paulo', 41: 'Paraná', 42: 'Santa Catarina',
-          43: 'Rio Grande do Sul', 50: 'Mato Grosso Sul', 51: 'Mato Grosso',
+          43: 'Rio Grande do Sul', 50: 'Mato Grosso do Sul', 51: 'Mato Grosso',
           52: 'Goiás', 53: 'Distrito Federal'}
 
 
@@ -25,12 +25,13 @@ def job():
     content = request.content.decode('utf8').replace('var database=', '')
     data = json.loads(content)
 
-    df = pd.DataFrame(None, columns=['date', 'state', 'suspects', 'refuses', 'cases'])
+    df = pd.DataFrame(None, columns=['date', 'hour', 'state',
+                                     'suspects', 'refuses', 'cases'])
 
     for record in data['brazil']:
-        date = f"{record['date']} {record['time']}"
-        date = datetime.strptime(date, '%d/%m/%Y %H:%M')
-        date = datetime.strftime(date, '%Y-%m-%d %H:%M')
+        confirmed_at = datetime.strptime(record['date'], '%d/%m/%Y')
+        confirmed_at = datetime.strftime(confirmed_at, '%Y-%m-%d')
+        hour = record['time']
 
         for value in record['values']:
             state = STATES[int(value['uid'])]
@@ -39,7 +40,8 @@ def job():
             refuses = get_key_value('refuses', value)
             cases = get_key_value('cases', value)
 
-            df = df.append(dict(zip(df.columns, [date, state, suspects, refuses, cases])), ignore_index=True)
+            df = df.append(dict(zip(df.columns, [confirmed_at, hour, state, suspects, refuses, cases])),
+                           ignore_index=True)
 
-    df.to_csv('data/brazil_covid19.csv', index=False)
+    df.to_csv('data/brazil_covid19.csv', index=False, sep=';')
     print("Done! The time is: %s" % datetime.now())
